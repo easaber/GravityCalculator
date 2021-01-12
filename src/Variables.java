55f0d553963ed501f498a3ad6b1bdc9c1b2df0e7
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-01-12 17:03:42 +0000

    First commit

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+import java.lang.System;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        int cake = (age > 18) ? 20 : 0;
+
+        System.out.println(cake);
+
+
+
+    }
+
+
+
+
+
+}
